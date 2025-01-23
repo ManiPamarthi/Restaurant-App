04261a55cac0473fe468d8b8e1691f12dcf0f305
Mani Pamarthi <manip@codestellartech.com>
2025-01-23 21:06:58 +0530

    update

---
+++ src/features/menu/components/hero-section.tsx
@@ -0,0 +1,30 @@
+import { IoRestaurantOutline } from "react-icons/io5";
+import { FiSearch, FiShoppingCart } from "react-icons/fi";
+import React from "react";
+import Image from "next/image";
+
+const HeroSection = ({ searchQuery, setSearchQuery }) => {
+  return (
+    <>
+        <div className="absolute inset-0">
+          <Image
+            src="/images/menuhero.png"
+            alt="Restaurant Interior"
+            className="object-cover w-full h-full"
+            fill={true}
+            priority
+          />
+        </div>
+        <div className="absolute inset-0 bg-black bg-opacity-50">
+          <div className="container mx-auto px-6 h-full flex items-center">
+            <div className="text-white">
+              <h1 className="text-5xl md:text-7xl font-serif mb-4">La Maison</h1>
+              <p className="text-xl md:text-2xl font-light">Where culinary dreams come true</p>
+            </div>
+          </div>
+        </div>
+    </>
+  );
+};
+
+export default HeroSection;
